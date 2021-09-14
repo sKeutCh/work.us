9d1b06057cfc2432098bd80e9df9cbf151ce2184
sKeutCh <90134535+sKeutCh@users.noreply.github.com>
2021-09-14 09:23:15 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+f = int(input())
+x = (a*b-c)
+z = (f-d)
+if z == 0:
+    print('делить на ноль нельзя')
+else:
+    print(x/z)
