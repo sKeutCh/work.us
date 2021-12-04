cec1b0e5c5de5e02d8b3eb62da54d6f7367ad8ba
ylana <ylanaskr15@gmail.com>
2021-12-05 01:23:14 +0300

    add file

--- dz5_binary_search/test.py
+++ dz5_binary_search/test.py
@@ -1,12 +1,12 @@
-from random import random
+import random
 
-from dz5_binary_search.bin_search import bin_search
+from bin_search import bin_search
 
-assert bin_search([], int(random() * 10**3)) is None, 'error with empty list'
+assert bin_search([], random.randint(0, 1)) is None, 'error with empty list'
 
 one_length_list = [123]
-assert bin_search(one_length_list, 123) == 0, '123 not in list [123]'
-assert bin_search(one_length_list, 1) is None, '1 in list [123]'
+assert bin_search(one_length_list, 123) == 0, 'another key'
+assert bin_search(one_length_list, 1) is None
 
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # array = list(range(10))
 assert bin_search(array, 93) is None, 'error because 93 is not in list'
