a849d5a670d4233b3c46482d82bbf4ac2a80b5a1
ylana <ylanaskr15@gmail.com>
2021-12-11 11:32:12 +0300

    add file

--- dz5_binary_search/bin_search.py
+++ dz5_binary_search/bin_search.py
@@ -9,12 +9,16 @@ def bin_search(array, key):
     lower_bound = 0
     upper_bound = len(array) - 1
 
-    while lower_bound < upper_bound:
+    while lower_bound <= upper_bound:
         middle = (lower_bound + upper_bound) // 2
         if array[middle] == key:
+            for n in range(middle + 1):
+                if array[n] == array[middle] and n < middle:
+                    middle = n
             return middle
         elif array[middle] > key:
             upper_bound = middle - 1
         elif array[middle] < key:
             lower_bound = middle + 1
     return None
+
--- dz5_binary_search/test.py
+++ dz5_binary_search/test.py
@@ -11,3 +11,11 @@ assert bin_search(one_length_list, 1) is None
 array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # array = list(range(10))
 assert bin_search(array, 93) is None, 'error because 93 is not in list'
 assert bin_search(array, 5) == 5, 'key index enable is not found'
+
+assert bin_search([-1, 0, 42], 0) == 1
+assert bin_search([-6, -5, -4, -3, -2, -1], -4) == 2
+assert bin_search([1, 2, 3, 4, 5], 4) == 3
+assert bin_search([1, 2, 3, 4, 5], 7) is None
+assert bin_search([42, 42, 42, 42, 42], 42) == 0
+assert bin_search([42, 42, 42, 42, 43], 43) == 4
+assert bin_search([-2, -2, -1, 0, 1, 1, 2, 2], 1) == 4
