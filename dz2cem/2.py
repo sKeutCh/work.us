36afedd5bbe2607974a5169440dc8ae9cc2d2120
ylana <ylanaskr15@gmail.com>
2022-03-26 13:22:14 +0300

    changed file

--- dz2cem/2.py
+++ dz2cem/2.py
@@ -1,34 +1,16 @@
-import math
+from fraction import Fraction
 
 
-class Fraction:
-    def __init__(self, nume=0, denom=1):
-        self.top = nume
-        self.bottom = denom
+class IrreducibleFraction(Fraction):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.reduce()
 
-    def input(self):
-        self.top = int(input("Введите числитель: "))
-        self.bottom = int(input("Введите знаменатель: "))
-        if self.bottom == 0:
-            raise ValueError("Знаменатель не равен 0")
-
-    def __str__(self):
-        return str(self.top)+"/"+str(self.bottom)
-
-    def reduce(self):
-        p = math.gcd(self.top, self.bottom)
-        return str(self.bottom / p)+'/'+str(self.bottom / p)
+    def input(self, *args, **kwargs):
+        super().input(*args, **kwargs)
+        self.reduce()
 
 
-p = Fraction()
-print(p)
-
-b1 = Fraction()
-print(b1)
-
-b2 = Fraction(3, 4)
-print(b2)
-
-b3 = Fraction()
-b3.input()
-print(b3)
+a = IrreducibleFraction()
+a.input()
+print(a)
--- dz2cem/fraction.py
+++ dz2cem/fraction.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Fraction:
     def __init__(self, nume=0, denom=1):
         self.top = nume
@@ -12,13 +15,10 @@ class Fraction:
     def __str__(self):
         return str(self.top)+"/"+str(self.bottom)
 
+    def __eq__(self, other) -> bool:
+        return self.top == other.top and self.bottom == other.bottom
 
-b1 = Fraction()
-print(b1)
-
-b2 = Fraction(3, 4)
-print(b2)
-
-b3 = Fraction()
-b3.input()
-print(b3)
+    def reduce(self):
+        p = math.gcd(self.top, self.bottom)
+        self.top = self.top // p
+        self.bottom = self.top // p
